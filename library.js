1a14902eb53136ff66cb091ca42b6d4ec1f9e612
SonathanJim <sonathanjim@gmail.com>
2024-12-10 13:21:35 +0000

    add dots button and dropdown menu

--- library.js
+++ library.js
@@ -26,10 +26,17 @@ function displayBooks() {
         //construct content for card
         const content = 
             `<div class="card">
-                <h3>Title:</h3><p class="title">${Book.title}</p>
-                <h3>Author:</h3><p class="author">${Book.author}</p>
-                <h3>Pages:</h3><p class="pages">${Book.pages}</p>
-                <h3>Read?:</h3><p class="read">${(() => {if (Book.read === true) {return 'yes';} else {return 'no';};})()}</p>
+                <button class="dropdown-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>dots-horizontal</title><path d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" /></svg></button>
+                <div class="dropdown-menu">
+                    <a href="" class="dropdown-item" id="remove-book">remove book</a>
+                    <a href="" class="dropdown-item" id="add-notes">add notes</a>
+                </div>
+                <div class="card-content">
+                    <h3>Title:</h3><p class="title">${Book.title}</p>
+                    <h3>Author:</h3><p class="author">${Book.author}</p>
+                    <h3>Pages:</h3><p class="pages">${Book.pages}</p>
+                    <h3>Read?:</h3><p class="read">${(() => {if (Book.read === true) {return 'yes';} else {return 'no';};})()}</p>
+                </div>
             </div>`
             ;
             //add card to container
@@ -47,7 +54,7 @@ function addBookToLibrary(title, author, pages, read) {
 //select modal and buttons
 const modal = document.getElementById('modal');
 const openModal = document.querySelector('.open-button');
-const closeModal = document.querySelector('.close-button')
+const closeModal = document.querySelector('.close-button');
 const addBookForm = document.getElementById('addBookForm');
 
 openModal.addEventListener('click', () => {
@@ -58,7 +65,6 @@ openModal.addEventListener('click', () => {
 closeModal.addEventListener('click', () => {
     modal.close();
 })
-
 
 addBookForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -72,8 +78,6 @@ addBookForm.addEventListener('submit', (e) => {
         alert ('Please provide a title and author')
     } else {
         addBookToLibrary(title.value, author.value, pages.value, read.value);
-        modal.close;
+        modal.close();
     }
 })
-
-
