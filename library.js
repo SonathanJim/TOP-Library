cd43dbc0c7f560f9e68f407a7f149d084b6c0cca
SonathanJim <sonathanjim@gmail.com>
2024-12-10 21:32:09 +0000

    add data-identifier to cards & fix read values

--- library.js
+++ library.js
@@ -25,7 +25,7 @@ function displayBooks() {
 
         //construct content for card
         const content = 
-            `<div class="card">
+            `<div class="card" data-identifier="${myLibrary.indexOf(Book)}">
                 <button class="dropdown-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>dots-horizontal</title><path d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" /></svg></button>
                 <div class="dropdown-menu">
                     <a href="" class="dropdown-item" id="remove-book">remove book</a>
@@ -35,7 +35,7 @@ function displayBooks() {
                     <h3>Title:</h3><p class="title">${Book.title}</p>
                     <h3>Author:</h3><p class="author">${Book.author}</p>
                     <h3>Pages:</h3><p class="pages">${Book.pages}</p>
-                    <h3>Read?:</h3><p class="read">${(() => {if (Book.read === 'on') {return 'yes';} else {return 'no';};})()}</p>
+                    <h3>Read?:</h3><p class="read">${(() => {if (Book.read === true) {return 'yes';} else {return 'no';};})()}</p>
                 </div>
             </div>`
             ;
@@ -81,7 +81,7 @@ addBookForm.addEventListener('submit', (e) => {
     if (title.value == '' || author.value == '') {
         alert ('Please provide a title and author')
     } else {
-        addBookToLibrary(title.value, author.value, pages.value, read.value);
+        addBookToLibrary(title.value, author.value, pages.value, read.checked);
         modal.close();
     }
 })
